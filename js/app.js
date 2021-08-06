e035108422a39e991903efad04fcc6b8917ad869
rohan-679 <76933542+rohan-679@users.noreply.github.com>
2021-08-06 13:47:12 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// ***********************************************tilt effect****************************
+
+// card 1
+VanillaTilt.init(document.querySelector(".card-first"), {
+  max: 20,
+  speed: 900,
+});
+
+// It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".card-first"));
+
+// card 2
+VanillaTilt.init(document.querySelector(".card-second"), {
+  max: 20,
+  speed: 900,
+});
+
+// It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".card-second"));
+
+// card 3
+VanillaTilt.init(document.querySelector(".card-third"), {
+  max: 20,
+  speed: 900,
+});
+
+// It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".card-third"));
+
+// ***********************************************tilt effect****************************
